6d6655a76904e0cf3c474dce929ed75a53ef6866
SergeiLutsev <snlutsev@outlook.com>
2019-08-04 08:46:16 -0400

    Add spiner

--- 9-forkify/src/js/models/Search.js
+++ 9-forkify/src/js/models/Search.js
@@ -10,10 +10,10 @@ export default class Search {
 
     async getResult() {
         const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key ='84b8096a36326982d3942b20011ce9d4';//'462b1cc8d4f2730081462fbc65136320'; 
+        const key ='462b1cc8d4f2730081462fbc65136320';//'84b8096a36326982d3942b20011ce9d4';//'462b1cc8d4f2730081462fbc65136320'; 
         try {
            // const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-            console.log(res);
+           // console.log(res);
             
             const res = tempData;
             this.results = res.data.recipes;
--- 9-forkify/src/js/views/base.js
+++ 9-forkify/src/js/views/base.js
@@ -1,5 +1,29 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
-    searchResultList: document.querySelector('.results__list')
-}
+    searchResultList: document.querySelector('.results__list'),
+    searchRes: document.querySelector('.results')
+};
+
+export const elemrntString = {
+    loader: 'loader'
+};
+
+export const renderLoader = (parent) =>{
+    const loader =`
+        <div class="${elemrntString.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    
+    parent.insertAdjacentHTML("afterbegin",loader);
+};
+
+export const clearLoader =()=>{
+   const loder = document.querySelector('.'+elemrntString.loader);
+   if(loder){
+       loder.parentElement.removeChild(loder);
+   }
+};
