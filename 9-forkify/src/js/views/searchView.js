da6e2211c1f726d5a91fa4c5a1a0b05238d74f11
SergeiLutsev <snlutsev@outlook.com>
2019-08-04 10:31:54 -0400

    Add Pagination

--- 9-forkify/src/js/views/searchView.js
+++ 9-forkify/src/js/views/searchView.js
@@ -10,6 +10,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResultList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 }
 
 
@@ -52,6 +53,45 @@ const renderRecipe = (recipe) => {
     elements.searchResultList.insertAdjacentHTML('beforeend', markup);
 }
 
-export const renderResults = (recipes) => {
-    recipes.forEach(renderRecipe);
+// type: 'prev' or 'next'
+const createButton = (page, type) => `
+
+    <button class="btn-inline results__btn--${type}" data-goto=${(type ==='prev')? page-1 : page+1}>
+        <span>Page ${(type ==='prev')? page-1 : page+1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${ type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+    </button>
+`;
+
+const renderBattons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+    let button;
+    if (page === 1 && pages > 1) {
+        // Only button to go to next page
+        button = createButton(page, 'next');
+    } else if (page < pages) {
+        //both Button
+        button = `
+                ${createButton(page,'prev')}
+                ${createButton(page,'next')}
+                `;
+
+    } else if (page === pages && pages > 1) {
+        //only button to go to priviose page
+        button = createButton(page, 'prev');
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // render results of current page
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // render pagination buttons
+    renderBattons(page, recipes.length, resPerPage);
 };
